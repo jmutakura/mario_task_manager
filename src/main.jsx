576eb5c0a022833abd5a0bda8edff2176891766f
jmutakura <jmutakura@gmail.com>
2023-02-28 16:28:29 -0600

    Managed to have the theme figured out

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+//extend theme
+const colors = {
+	brand: {
+		600: 'red',
+	},
+};
+
+const fonts = {
+	body: 'Tahoma',
+	heading: 'Courier New',
+};
+
+const theme = extendTheme(colors, fonts);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>
