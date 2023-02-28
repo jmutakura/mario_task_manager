5741bbe5aa203c4862398c6e5c3c8467dba5c2f0
jmutakura <jmutakura@gmail.com>
2023-02-28 11:14:03 -0600

    Made changes to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,75 @@
+import {
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+	List,
+	ListItem,
+	ListIcon,
+} from '@chakra-ui/react';
+import {
+	ChatIcon,
+	CheckCircleIcon,
+	EmailIcon,
+	StarIcon,
+	WarningIcon,
+} from '@chakra-ui/icons';
+
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+	return (
+		<Tabs mt='40px' p='20px' colorScheme='green' variant='enclosed'>
+			<TabList>
+				<Tab _selected={{ color: 'white', bg: 'green.400' }}>Account Info</Tab>
+				<Tab _selected={{ color: 'white', bg: 'green.400' }}>Task History</Tab>
+			</TabList>
+
+			<TabPanels>
+				<TabPanel>
+					<List spacing={4}>
+						<ListItem>
+							<ListIcon as={EmailIcon} />
+							Email: mario@email.com.dev
+						</ListItem>
+						<ListItem>
+							<ListIcon as={ChatIcon} />
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={StarIcon} />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+					</List>
+				</TabPanel>
+				<TabPanel>
+					<List spacing={4}>
+						<ListItem>
+							<ListIcon as={CheckCircleIcon} color='teal.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={CheckCircleIcon} color='teal.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={WarningIcon} color='red.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={CheckCircleIcon} color='teal.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={WarningIcon} color='red.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+						<ListItem>
+							<ListIcon as={CheckCircleIcon} color='teal.400' />
+							Lorem ipsum dolor sit amet consectetur.
+						</ListItem>
+					</List>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
+	);
 }
