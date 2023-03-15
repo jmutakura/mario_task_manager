b361f20ce3b63646a008202ba129a21a6605d2c6
Jonathan Mutakura <jonathan.mutakura@pivotfurniture.com>
2023-03-15 00:05:39 -0600

    Added headers to the fetch request

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -61,7 +61,9 @@ export default function Dashboard() {
 }
 
 export const taskLoader = async () => {
-	const res = await fetch('../../data/db.json');
+	const res = await fetch('../../data/db.json', {
+		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+	});
 	const json_res = await res.json();
 
 	return json_res;
